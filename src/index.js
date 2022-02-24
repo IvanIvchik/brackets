58076e901c67c893b35eb80f7e5a4d6ad589c28c
Ivan Ivchik <63555972+IvanIvchik@users.noreply.github.com>
2022-02-24 13:41:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ if (str.length % 2 !== 0) return false;
+ if (str.length === 0) return true;
+
+ let arrStr = str.split('');
+            
+ for (let j = 0; j < arrStr.length; j++) {
+     for (let i = 0; i < bracketsConfig.length; i++) {
+         if (arrStr[j] == bracketsConfig[i][0] && arrStr[j + 1] == bracketsConfig[i][1]) {
+              arrStr.splice(j, 2);
+              j = - 1;
+                }
+             }
+         }
+     return arrStr.length == 0;
 }
